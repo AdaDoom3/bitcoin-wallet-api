9ba473f251f0ae3dd2aa20a3773555e422d4c2fd
Michael Hardeman <mhardeman25@gmail.com>
2018-01-25 00:00:19 -0500

    maybe keypairs?

--- app/schemas/v1/wallet.js
+++ app/schemas/v1/wallet.js
@@ -1,10 +1,23 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const Schema = require('mongoose').Schema;
+const bitcoin = require('bitcoinjs-lib');
 
 var wallet = new Schema({
   name:  String,
   coin: {name: String, symbol: String},
-  keys: [{public: String, private: String}]
+  keys: [String]
 });
+
+wallet.statics.toKeyPair = function (wif){
+  return bitcoin.ECPair.fromWIF (wif);
+};
+
+wallet.methods.generateNewKeyPair = function(name, cb) {
+  this.keys.push(bitcoin.ECPair.makeRandom().toWIF());
+  this.save();
+};
+
+wallet.methods.allKeysAsECPairs = function(name, cb) {
+  return this.keys.map((wif) => wallet.statics.toKeyPair (wif));
+};
 
 module.exports = wallet;
